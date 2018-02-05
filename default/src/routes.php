08d57d7dac944fa962bb5478e4d3ac26f2e079e8
matmorel <pub.imagiin@gmail.com>
2018-02-05 11:10:28 +0100

    add login form

--- default/src/routes.php
+++ default/src/routes.php
@@ -5,10 +5,19 @@ use Slim\Http\Response;
 
 // Routes
 
-$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
+$app->get('/', function (Request $request, Response $response, array $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
 
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
+});
+
+$app->post('/login', function (Request $request, Response $response, array $args) {
+    // Sample log message
+    $this->logger->info("Slim-Skeleton '/login' route");
+
+    $body = $request->getParsedBody();
+    $body['is_logged'] = ($body['email'] === "toto@gmail.com" && $body['password'] === "pass");
+    return $this->renderer->render($response, 'login.phtml', $body);
 });
